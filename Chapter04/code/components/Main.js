7fbac317d0ea06efa6fad063b7a3e293a8b9252b
Jay Rajput <rajputjay901@gmail.com>
2023-01-17 03:00:38 +0530

    refactor: changed code for restaurant filter

--- Chapter04/code/components/Main.js
+++ Chapter04/code/components/Main.js
@@ -5,19 +5,38 @@ import Restaurants from "./Restaurants";
 export const Main = () => {
   const [restName, setRestName] = useState("");
   const [allRest, setAllRest] = useState(data);
+
+  const filterData = (restName, allRest, setAllRest) => {
+    let resData = allRest.filter((item) =>
+      item.data.name.toLowerCase().includes(restName)
+    );
+    setAllRest(resData);
+    return resData;
+  };
+  // let newData = filterData(restName, allRest, setAllRest);
+
+  const searchBtnClickHandler = (e) => {
+    setAllRest(filterData(restName, allRest, setAllRest));
+  };
   return (
+    // <></>
     <main className="main">
-      <input
-        type="text"
-        value={restName}
-        id="rest-name"
-        onChange={(e) => setRestName(e.target.value)}
-      />
+      <div className="search-container">
+        <input
+          type="text"
+          value={restName}
+          id="rest-name"
+          onChange={(e) => setRestName(e.target.value)}
+        />
+        <button onClick={searchBtnClickHandler}>search</button>
+      </div>
+
       <Restaurants
         restName={restName}
         allRest={allRest}
         setAllRest={setAllRest}
       />
+      {console.log({ restName, allRest })}
     </main>
   );
 };
